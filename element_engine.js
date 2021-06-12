0a3eae38293f5924402cccb95039cda75c311055
Aashish <aasish.agarwal.75@gmail.com>
2021-06-12 21:09:55 +0530

    Base Version

---
+++ element_engine.js
@@ -0,0 +1,125 @@
+var element_template =
+`
+<div class="row">
+<div class="instructions col-md-12">
+<p>
+<h1>Instructions:</h1>																
+Pay attention to each box. 																		
+<br>Do not analyze each word; just get a sense of each box. 																		
+<br>Score each of the four boxes in each row from most to least as it describes you: 																		
+<ul>
+<li>Least Likely</li>
+<li>Somewhat likely	</li>
+<li>A lot likely</li>	
+<li>Most Likely</li>
+</ul>
+</p>
+</div>
+</div>
+<div class="row">
+{{#qualities_container}}
+    <div class="mybox col-md-3">
+
+    <ul>
+        {{#qualities}}
+        <li>
+        {{qname}}}}
+        </li>
+        {{/qualities}}
+    </ul>
+    <select id="{{boxname}}" class="select-boxes">
+        <option value="0">Your Choice</option>
+        <option value="1">Least Likely</option>
+        <option value="2">Somewhat likely</option>
+        <option value="3">A lot likely</option>
+        <option value="4">Most Likely</option>
+    </select>
+    <br><br>
+
+    </div>
+{{/qualities_container}}
+
+<div class="col-md-12">
+<p>
+<br>
+<button onclick="ShowResults()" style="float: right;">Show Results</button>
+<br>
+<div id="alertcontainer"></div>
+</p>
+
+
+</div>
+</div>
+`;
+
+var result_template =
+`
+<table style="font-family: arial, sans-serif; border-collapse: collapse; width: 80%;">
+<tr>
+<td style="color: white; background-color:red; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{red}}</td>
+
+<td style="color: white; font-size: medium; background-color:red; border: 1px solid #dddddd;  text-align: left; padding: 8px;">
+
+<strong>Personality Style: Mover</strong>
+<br><strong>Core Value: Freedom</strong>
+<br>The Mover personality style is courageous, exploratory, and playful. 
+Movers seek action and adventure
+They crave variety and enjoy improvising.
+Movers are good at thinking on their feet.
+They automatically find the fastest way to do things and make them fun. 
+They change course as often as is needed and aren't likely to let bumps in the road slow them down.
+
+</td>
+</tr>
+
+<tr>
+
+<td style="color: white; background-color:green; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{green}}</td>
+
+<td style="color: white; font-size: medium; background-color:green; border: 1px solid #dddddd;  text-align: left; padding: 8px;">
+<strong>Personality Style: Thinker</strong>
+<br><strong>Core Value: Competency</strong>
+<br>The Thinker personality style is curious, logical, and self-sufficient. 
+Thinkers seek clarity and knowledge. 
+They prefer to make calculated decisions. 
+Thinkers explore all aspects of an issue and can't help but suggest new ways of doing things. 
+They need time to think before making decisions. They can be fiercely independent and value their privacy
+</td>
+
+</tr>
+
+<tr>
+
+<td style="color: white; background-color:blue; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{blue}}</td>
+
+<td style="color: white; font-size: medium; background-color:blue; border: 1px solid #dddddd;  text-align: left; padding: 8px;">
+<strong>Personality Style: Connector</strong>
+<br><strong>Core Value: Relationship</strong>
+<br>The Connector personality style is considerate, cooperative, and encouraging. 
+Connectors seek harmony and personal connection. 
+They prefer to make decisions that feel good and are in alignment with their values. 
+Connectors naturally interact with others and connect meaning to events. They excel at recognizing strengths in others and place a high importance on person growth. 
+</td>
+
+</tr>
+
+<tr>
+
+<td style="color: black; background-color:yellow; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{yellow}}</td>
+
+
+<td style="color: black; font-size: medium; background-color:yellow; border: 1px solid #dddddd;  text-align: left; padding: 8px;">
+<strong>Personality Style: Planner</strong>
+<br><strong>Core Value: Responsibility</strong>
+<br>The Planner personality style is organized, prepared, and dependable. Planners seek order and fairness. 
+They crave consistency and having things in their place. 
+Planners are naturally able to distinguish right from wrong. 
+They like to keep their personal space well-tended and pay attention to details. 
+They seek a sense of completion and enjoy crossing items off a list. 
+</td>
+</tr>
+</table>
+</table>
+
+`;
+
---
+++ function.js
@@ -0,0 +1,100 @@
+var QualitySet = [];
+
+var element = {};element.qualities = []; element.boxname ="A";	element.qualities.push({"qname" :  "Active"});	element.qualities.push({"qname" :  "Variety"});	element.qualities.push({"qname" :  "Sports "});	element.qualities.push({"qname" :  "Opportunities"});	element.qualities.push({"qname" :  "Flexible"});	element.qualities.push({"qname" :  "Spontaneous "});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="B";	element.qualities.push({"qname" :  "Organized"});	element.qualities.push({"qname" :  "Planned"});	element.qualities.push({"qname" :  "Neat"});	element.qualities.push({"qname" :  "Parental"});	element.qualities.push({"qname" :  "Traditional "});	element.qualities.push({"qname" :  "Responsible"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="C";	element.qualities.push({"qname" :  "Warm"});	element.qualities.push({"qname" :  "Helpful"});	element.qualities.push({"qname" :  "Friends"});	element.qualities.push({"qname" :  "Authentic "});	element.qualities.push({"qname" :  "Harmonious "});	element.qualities.push({"qname" :  "Compassionate"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="D";	element.qualities.push({"qname" :  "Learning"});	element.qualities.push({"qname" :  "Science "});	element.qualities.push({"qname" :  "Quiet"});	element.qualities.push({"qname" :  "Versatile "});	element.qualities.push({"qname" :  "Inventive "});	element.qualities.push({"qname" :  "Competent"});	QualitySet.push(element);
+
+var element = {};element.qualities = []; element.boxname ="E";	element.qualities.push({"qname" :  "Curious"});	element.qualities.push({"qname" :  "Ideas"});	element.qualities.push({"qname" :  "Questions "});	element.qualities.push({"qname" :  "Knowledge"});	element.qualities.push({"qname" :  "Problem Solver"});	element.qualities.push({"qname" :  "Conceptual"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="F";	element.qualities.push({"qname" :  "Caring"});	element.qualities.push({"qname" :  "People Oriented "});	element.qualities.push({"qname" :  "Unique"});	element.qualities.push({"qname" :  "Empathetic"});	element.qualities.push({"qname" :  "Feelings"});	element.qualities.push({"qname" :  " Communicative"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="G";	element.qualities.push({"qname" :  "Orderly "});	element.qualities.push({"qname" :  "On-time "});	element.qualities.push({"qname" :  "Honest "});	element.qualities.push({"qname" :  "Stable "});	element.qualities.push({"qname" :  "Sensible "});	element.qualities.push({"qname" :  "Dependable"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="H";	element.qualities.push({"qname" :  "Action"});	element.qualities.push({"qname" :  "Challenges "});	element.qualities.push({"qname" :  "Competitive "});	element.qualities.push({"qname" :  "Impetuous"});		QualitySet.push(element);
+
+var element = {};element.qualities = []; element.boxname ="I";	element.qualities.push({"qname" :  "Helpful"});	element.qualities.push({"qname" :  "Trustworthy"});	element.qualities.push({"qname" :  "Loyal"});	element.qualities.push({"qname" :  "Conservative "});	element.qualities.push({"qname" :  "Dependable"});	element.qualities.push({"qname" :  "Organized"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="J";	element.qualities.push({"qname" :  "Kind"});	element.qualities.push({"qname" :  "Understanding "});	element.qualities.push({"qname" :  "Giving"});	element.qualities.push({"qname" :  "Devoted"});	element.qualities.push({"qname" :  "Warm "});	element.qualities.push({"qname" :  "Poetic"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="K";	element.qualities.push({"qname" :  "Playful"});	element.qualities.push({"qname" :  "Quick"});	element.qualities.push({"qname" :  "Adventurous"});	element.qualities.push({"qname" :  "Open Minded "});	element.qualities.push({"qname" :  "Confrontative"});	element.qualities.push({"qname" :  "Independent"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="L";	element.qualities.push({"qname" :  "Independent "});	element.qualities.push({"qname" :  "Competent "});	element.qualities.push({"qname" :  "Why Questions "});	element.qualities.push({"qname" :  "Exploring"});	element.qualities.push({"qname" :  "Theoretical "});	element.qualities.push({"qname" :  "Ingenious"});	QualitySet.push(element);
+
+var element = {};element.qualities = []; element.boxname ="M";	element.qualities.push({"qname" :  "Follow Rules"});	element.qualities.push({"qname" :  "Useful"});	element.qualities.push({"qname" :  "SaveMoney "});	element.qualities.push({"qname" :  "Concerned"});	element.qualities.push({"qname" :  "Procedural"});	element.qualities.push({"qname" :  "Cooperative"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="N";	element.qualities.push({"qname" :  "Active"});	element.qualities.push({"qname" :  "Free"});	element.qualities.push({"qname" :  "Winning"});	element.qualities.push({"qname" :  "Daring"});	element.qualities.push({"qname" :  "Impulsive"});	element.qualities.push({"qname" :  "Risk Taker"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="O";	element.qualities.push({"qname" :  "Sharing"});	element.qualities.push({"qname" :  "Getting Along "});	element.qualities.push({"qname" :  "Tender"});	element.qualities.push({"qname" :  "Inspirational"});	element.qualities.push({"qname" :  "Dramatic"});	element.qualities.push({"qname" :  "Feelings"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="P";	element.qualities.push({"qname" :  "Thinking"});	element.qualities.push({"qname" :  "Solving Problems"});	element.qualities.push({"qname" :  "Perfectionistic "});	element.qualities.push({"qname" :  "Determined "});	element.qualities.push({"qname" :  "Complex "});	element.qualities.push({"qname" :  "Composed"});	QualitySet.push(element);
+
+var element = {};element.qualities = []; element.boxname ="Q";	element.qualities.push({"qname" :  "Puzzles "});	element.qualities.push({"qname" :  "Seeking Info"});	element.qualities.push({"qname" :  "Making Sense"});	element.qualities.push({"qname" :  "Rational"});	element.qualities.push({"qname" :  "Principled "});	element.qualities.push({"qname" :  "Philosophical "});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="R";	element.qualities.push({"qname" :  "Social Causes "});	element.qualities.push({"qname" :  "Happy Endings "});	element.qualities.push({"qname" :  "Easy Going"});	element.qualities.push({"qname" :  "Approachable "});	element.qualities.push({"qname" :  "Affectionate "});	element.qualities.push({"qname" :  "Sympathetic"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="S";	element.qualities.push({"qname" :  "Exciting"});	element.qualities.push({"qname" :  "Lively"});	element.qualities.push({"qname" :  "Hands On"});	element.qualities.push({"qname" :  "Skillful"});	element.qualities.push({"qname" :  "Courageous"});	element.qualities.push({"qname" :  "On Stage"});	QualitySet.push(element);
+var element = {};element.qualities = []; element.boxname ="T";	element.qualities.push({"qname" :  "Pride"});	element.qualities.push({"qname" :  "Tradition"});	element.qualities.push({"qname" :  "Do Things Right "});	element.qualities.push({"qname" :  "Conventional "});	element.qualities.push({"qname" :  "Orderly"});	element.qualities.push({"qname" :  "Careful"});	QualitySet.push(element);
+
+
+
+function ShowTest()
+{
+    $("#explanation").hide();
+    $("#element_container").hide();
+    
+    var dataForelementTemplate = { "qualities_container" : QualitySet };
+    var elements_html = Mustache.render(element_template, dataForelementTemplate);
+    document.getElementById("element_container").innerHTML = elements_html;
+    $("#element_container").show();
+    $('#element_container').scrollTop();
+
+    var maxheight = 0;
+    $( ".mybox" ).each(function( index ) {
+    if ( maxheight < $( this ).height() )
+        {
+            maxheight = $( this ).height() ;
+        }
+    });
+    
+    $( ".mybox" ).height(maxheight );
+}
+
+function ShowResults()
+{
+    var RED = ["A", "H" , "K" , "N" , "S"];
+    var GREEN = ["D", "E" , "L" , "P" , "Q"];
+    var BLUE = ["C", "F" , "O" , "J" , "R"];
+    var YELLOW = ["B", "G" , "I" , "M" , "T"];
+
+    var color_groups = [];
+    color_groups.push( {"code": "red" , "values" : RED});
+    color_groups.push( {"code": "green" , "values" : GREEN});
+    color_groups.push( {"code": "blue" , "values" : BLUE});
+    color_groups.push( {"code": "yellow" , "values" : YELLOW});
+
+    var result_data = {};
+    result_data.red = 0;
+    result_data.green = 0;
+    result_data.blue = 0;
+    result_data.yellow = 0;
+
+    var i;
+    var j;
+    for (i = 0; i < color_groups.length; i++) 
+    {
+        code = color_groups[i].code;
+        for (j = 0; j < color_groups[i].values.length; j++)
+        {
+            if ( value < 1 )
+            {
+               // alert("Please provide your assessment for all sections");
+               $("#alertcontainer").html(`<div class="alert alert-danger alert-dismissible show" role="alert">
+               Please provide your assessment for all sections
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button></div>`);
+
+                return;
+            } 
+            var boxid  = color_groups[i].values[j];
+            var value =  $("#" + boxid).val();
+            result_data[code] += parseInt(value);
+        } 
+    }
+
+    var results_html = Mustache.render(result_template, result_data);
+
+    $("#explanation").show();
+    document.getElementById("results").innerHTML = results_html;
+    document.getElementById("element_container").innerHTML = "";
+}
